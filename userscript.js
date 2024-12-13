52d7dd7436003432377cf72317e7171062df7d23
ShadowMods <165445930+ShadowMods1@users.noreply.github.com>
2024-12-12 20:29:25 -0500

    Create userscript.js

---
+++ userscript.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name        AIMx
+// @namespace   Script Runner Pro
+// @match       https://voxiom.io/*
+// @run-at      document-start
+// @grant       none
+// @version     0.1
+// @author      ShadowMods
+// @description Unlock advanced gameplay enhancements with our userscript, featuring customizable aimbot, triggerbot, ESP, recoil control, and more, all through an intuitive and user-friendly interface.
+// @license     Version 2.0
+// @require     https://raw.githubusercontent.com/ShadowMods1/AIMx-Voxiom.IO/refs/heads/main/lil-gui.js
+// @require     https://raw.githubusercontent.com/ShadowMods1/AIMx-Voxiom.IO/refs/heads/main/three.js
+// @icon        https://i.postimg.cc/y87h3x7y/0dc15569002a714e09ef3b03da9ac43d-removebg-preview.png
+// @downloadURL 
+// @updateURL 
+// ==/UserScript==
