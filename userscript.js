2907598f0f9aa7f845de519f085ba59a96251c9a
ShadowMods <165445930+ShadowMods1@users.noreply.github.com>
2024-12-12 20:30:24 -0500

    Update userscript.js

--- userscript.js
+++ userscript.js
@@ -7,10 +7,10 @@
 // @version     0.1
 // @author      ShadowMods
 // @description Unlock advanced gameplay enhancements with our userscript, featuring customizable aimbot, triggerbot, ESP, recoil control, and more, all through an intuitive and user-friendly interface.
-// @license     Version 2.0
+// @license     Apache License 2.0
 // @require     https://raw.githubusercontent.com/ShadowMods1/AIMx-Voxiom.IO/refs/heads/main/lil-gui.js
 // @require     https://raw.githubusercontent.com/ShadowMods1/AIMx-Voxiom.IO/refs/heads/main/three.js
 // @icon        https://i.postimg.cc/y87h3x7y/0dc15569002a714e09ef3b03da9ac43d-removebg-preview.png
-// @downloadURL 
-// @updateURL 
+// @downloadURL https://raw.githubusercontent.com/ShadowMods1/AIMx-Voxiom.IO/refs/heads/main/userscript.js
+// @updateURL   https://raw.githubusercontent.com/ShadowMods1/AIMx-Voxiom.IO/refs/heads/main/userscript.js
 // ==/UserScript==
